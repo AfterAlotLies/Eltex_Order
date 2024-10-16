b852f4d0e05933050c7c69630590c194f842b193
Vyacheslav <guzxaroh@gmail.com>
2024-10-16 18:34:04 +0700

    init commit

---
+++ HomeWork#Order/Application/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  HomeWork#Order
+//
+//  Created by Vyacheslav Gusev on 16.10.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {}
+
---
+++ HomeWork#Order/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork#Order
+//
+//  Created by Vyacheslav Gusev on 16.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
