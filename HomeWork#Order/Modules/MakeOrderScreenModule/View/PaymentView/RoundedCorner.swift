e3c9cb3030e2726d95551ecfeb4d449b141bdf20
Vyacheslav <guzxaroh@gmail.com>
2024-12-16 03:50:08 +0700

    implemented new module - "Make Order"

---
+++ HomeWork#Order/Modules/MakeOrderScreenModule/View/PaymentView/RoundedCorner.swift
@@ -0,0 +1,23 @@
+//
+//  RoundedCorner.swift
+//  HomeWork#Order
+//
+//  Created by Vyacheslav Gusev on 13.12.2024.
+//
+
+import SwiftUI
+
+struct RoundedCorner: Shape {
+    let radius: CGFloat
+    let corners: UIRectCorner
+
+    init(radius: CGFloat = .infinity, corners: UIRectCorner = .allCorners) {
+        self.radius = radius
+        self.corners = corners
+    }
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
